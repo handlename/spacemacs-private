b0b9bbb95627262aa6bf070502d8fda5805b60b2
handlename <nagata@handlena.me>
2016-01-24 15:59:13 +0900

    layer for perl

--- .spacemacs
+++ .spacemacs
@@ -36,6 +36,7 @@ values."
      ;; syntax-checking
      ;; version-control
      dired
+     my-perl
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
---
+++ private/my-perl/packages.el
@@ -0,0 +1,35 @@
+(defconst my:perl-packages
+  '(
+    cperl-mode
+    ))
+
+(defun my:perl/init-cperl-mode ()
+  (use-package cperl-mode
+    :defer t
+    :config
+    (progn
+      (setq cperl-electric-parens nil)
+
+      ;; use cperl-mode
+      (add-to-list 'auto-mode-alist '("\\.pl$" . cperl-mode))
+      (add-to-list 'auto-mode-alist '("\\.pm$" . cperl-mode))
+      (add-to-list 'auto-mode-alist '("\\.t$" . cperl-mode))
+      (add-to-list 'auto-mode-alist '("\\.psgi$" . cperl-mode))
+
+      ;; indent
+      (custom-set-variables
+       '(cperl-indent-level 4)
+       '(cperl-continued-statement-offset 4)
+       '(cperl-brace-offset -4)
+       '(cperl-label-offset -4)
+       '(cperl-indent-parens-as-block t)
+       '(cperl-close-paren-offset -4)
+       '(cperl-tab-always-indent t)
+       '(cperl-highlight-variables-indiscriminately t)
+       '(cperl-indent-subs-specially nil))
+      )))
+
+;; always use cperl-mode instead of perl-mode
+(defalias 'perl-mode 'cperl-mode)
+
+;;; packages.el ends here
