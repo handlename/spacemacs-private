66e040ed387747de2bd2503cd6129c3becae1080
handlename <nagata@handlena.me>
2016-09-02 14:26:53 +0900

    escape backslashes in environment variables.

--- scripts/init_shellenv.pl
+++ scripts/init_shellenv.pl
@@ -4,5 +4,7 @@ use warnings;
 
 for my $name (sort keys %ENV) {
     next if ! exists $ENV{$name};
-    printf qq{(setenv "%s" "%s")\n}, $name, $ENV{$name};
+    my $value = $ENV{$name};
+    $value =~ s/\\/\\\\/g;
+    printf qq{(setenv "%s" "%s")\n}, $name, $value;
 }
