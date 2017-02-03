69a7b550950725502f6f116894fc2b26b47a3738
handlename <nagata@handlena.me>
2017-02-03 19:00:10 +0900

    key bindings for perltidy

--- private/my-perl/packages.el
+++ private/my-perl/packages.el
@@ -6,6 +6,11 @@
 (defun my-perl/init-cperl-mode ()
   (use-package cperl-mode
     :defer t
+    :init
+    (evil-leader/set-key-for-mode 'cperl-mode
+      "fr" 'perltidy-region
+      "fd" 'perltidy-defun
+      "fb" 'perltidy-buffer)
     ))
 
 ;;; packages.el ends here
